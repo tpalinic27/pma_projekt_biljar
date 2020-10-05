2690c9c813fa8d3defa29df711858162f46d005d
tpalinic27 <72397111+tpalinic27@users.noreply.github.com>
2020-10-05 16:21:26 +0200

    Add files via upload

---
+++ Mapper/KluboviMapper.cs
@@ -0,0 +1,29 @@
+﻿using biljar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace biljar.Mapper
+{
+    public class KluboviMapper
+    {
+
+        public static Klubovi FromDatabase(DbModels.Klubovi k)
+        {
+            return new Klubovi(k.IdKlubovi, k.Ime, k.Lokacija, k.Kontakt);
+
+        }
+        public static DbModels.Klubovi ToDatabase(Klubovi k)
+        {
+            return new DbModels.Klubovi
+            {
+                IdKlubovi = k.Id_klubovi.HasValue ? k.Id_klubovi.Value : 0,
+                Ime = k.Ime,
+                Lokacija = k.Lokacija,
+                Kontakt = k.Kontakt,
+
+            };
+        }
+    }
+}
---
+++ Mapper/KorisniciMapper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using biljar.Controllers;
+using System.Threading.Tasks;
+using biljar.Models;
+
+
+namespace biljar.Mapper
+{
+    public class KorisniciMapper
+    {
+        public static Korisnik FromDatabase(DbModels.Korisnici k)
+        {
+
+            return new Korisnik(
+                k.IdKorisnici,
+                k.Email, 
+                k.Username,
+                k.Password,
+                k.Liga,
+                k.IdKlubovi
+                ); 
+
+        }
+        public static DbModels.Korisnici ToDatabase(Korisnik k)
+        {
+            return new DbModels.Korisnici
+            {
+                IdKorisnici = k.Id.HasValue ? k.Id.Value : 0,
+                Email = k.Email,
+                Username = k.Username,
+                Password = k.Password,
+                Liga = k.Liga,
+                IdKlubovi = k.KlubId
+
+            };
+        }
+    }
+}
