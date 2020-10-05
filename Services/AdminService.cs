bd5b5a2100ad90977fe5deecfcc2d13776cb1248
tpalinic27 <72397111+tpalinic27@users.noreply.github.com>
2020-10-05 16:24:18 +0200

    Add files via upload

---
+++ Services/AdminService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using biljar.Models;
+using biljar.Repositories;
+
+namespace biljar.Services
+{
+    public class AdminService
+    {
+        public AdminRepository _adminRepository;
+
+        public AdminService(AdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+        public IEnumerable<Korisnik> GetAll()
+        {
+            return _adminRepository.GetAll();
+        }
+
+        public IEnumerable<Models.Klubovi> GetAllKlubovi()
+        {
+
+            return _adminRepository.GetAllKlubovi();
+
+        }
+
+        public Klubovi GetKlub(string k)
+        {
+            return _adminRepository.GetKlub(k);
+        }
+
+        public void AddUser(Models.Korisnik k)
+        {
+            _adminRepository.AddUser(k);
+        }
+
+        //public IEnumerable<TaskToDo> GetTasks(int id)
+        //{
+        //    return _adminRepository.GetTasks(id);
+        //}
+        public async Task<Korisnik> Getuser(int id)
+        {
+            return await _adminRepository.Getuser(id);
+        }
+        public void UserEdit(Korisnik k)
+        {
+            _adminRepository.UserEdit(k);
+        }
+
+        public void DeleteKlub(int id)
+        {
+            _adminRepository.DeleteKlub(id);
+        }
+        public void DeleteKorisnik(int id)
+        {
+            _adminRepository.DeleteKorisnik(id);
+        }
+
+        public void AddKlub(Models.Klubovi klub)
+        {
+            _adminRepository.AddKlub(klub);
+
+        }
+        public void EditKlub(Models.Klubovi klub)
+        {
+            _adminRepository.EditKlub(klub);
+
+
+        }
+    }
+}
---
+++ Services/KorisnikService.cs
@@ -0,0 +1,61 @@
+﻿using biljar.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using biljar.Mapper;
+using biljar.Models;
+
+
+namespace biljar.Services
+{
+    public class KorisnikService
+    {
+        public KorisnikRepository _KorisnikRepository;
+
+        public KorisnikService(KorisnikRepository KorisnikRepository)
+        {
+            _KorisnikRepository = KorisnikRepository;
+        }
+        public IEnumerable<Korisnik> GetAll(int KorisnikId)
+        {
+            return _KorisnikRepository.GetAll(KorisnikId);
+        }
+        public async Task<Korisnik> GetAsync(int id)
+        {
+            return await _KorisnikRepository.GetAsync(id);
+        }
+        public void Save(Korisnik k)
+        {
+            _KorisnikRepository.Save(k);
+        }
+
+        public  Models.Korisnik GetKorisnik(int id)
+        {
+            return _KorisnikRepository.GetKorisnik(id);
+        }
+
+        public void Edit(Models.Korisnik k) 
+        {
+            _KorisnikRepository.Edit(k);
+        }
+
+        public DbModels.Korisnici GetKorisnikDB(int id)
+        {
+            
+            return _KorisnikRepository.GetKorisnikDB(id);
+        }
+
+        public IEnumerable<DbModels.Korisnici> GetAll2()
+        {
+          
+            return _KorisnikRepository.GetAll2();
+        }
+
+        public IEnumerable<Models.Klubovi> GetKlubovi()
+        {
+           return _KorisnikRepository.GetKlubovi();
+        }
+
+    }
+}
