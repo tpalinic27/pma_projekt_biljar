ced4484a6e315a307dbd1e832ac935017d3ec8c2
tpalinic27 <72397111+tpalinic27@users.noreply.github.com>
2020-10-05 16:17:31 +0200

    Add files via upload

---
+++ baza/dbo.Korisnici.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Korisnici] (
+    [Id_korisnici] INT          IDENTITY (1, 1) NOT NULL,
+    [Email]        VARCHAR (50) NOT NULL,
+    [Password]     VARCHAR (50) NOT NULL,
+    [Liga]         VARCHAR (50) NULL,
+    [Id_klubovi]   INT          NULL,
+    [Username] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Korisnici] PRIMARY KEY CLUSTERED ([Id_korisnici] ASC),
+    CONSTRAINT [FK_Korisnici_Klubovi] FOREIGN KEY ([Id_klubovi]) REFERENCES [dbo].[Klubovi] ([Id_klubovi])
+);
+
