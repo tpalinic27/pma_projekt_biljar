f696ab74fbe53a3ad10d1feffa03503861642dc6
tpalinic27 <72397111+tpalinic27@users.noreply.github.com>
2020-10-05 16:20:49 +0200

    Add files via upload

---
+++ DtoMappers/KorisnikDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using biljar.DbModels;
+using biljar.Models;
+using Newtonsoft.Json.Linq;
+
+namespace biljar.DtoMappers
+{
+    public static class KorisnikDto
+    {
+
+
+        public static Korisnik FromJson(JObject json)
+        {
+            var id = json["IdKorisnici"].ToObject<int?>();
+            var email = json["Email"].ToObject<string>();
+            var username = json["Username"].ToObject<string>();
+            var password = json["Password"].ToObject<string>();
+            var liga = json["Liga"].ToObject<string>();
+
+
+
+
+
+            return new Korisnik(id, email, username, password, liga,0);
+        }
+
+
+    }
+}
